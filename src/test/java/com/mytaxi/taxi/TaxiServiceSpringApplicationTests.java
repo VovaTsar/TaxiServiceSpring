d4f4e6e8ce653337a926d40bdb9bbc91c778d8c1
VovaTsar <dropttecttt@mail.ru>
2019-11-18 13:54:48 +0200

    Start project

---
+++ src/test/java/com/mytaxi/taxi/TaxiServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytaxi.taxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiServiceSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
